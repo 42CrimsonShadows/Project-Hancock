8aad776d629c40372fc7b13288aeeea66d45123f
Casey <casey@laurens-mini.boisestate.edu>
2019-05-29 12:49:06 -0600

    Added a Registration Page (No Constraints)

---
+++ Authentication/AuthenticationViewController.swift
@@ -0,0 +1,42 @@
+//
+//  AuthenticationViewController.swift
+//  Hancock
+//
+//  Created by Casey on 5/29/19.
+//  Copyright © 2019 Chris Ross. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class AuthenticationViewController: UIViewController,UITextFieldDelegate {
+    
+    
+    //MARK: -- Outlets
+    
+    //MARK: -- Actions
+    
+    //MARK: -- Variables
+    
+    //MARK: -- Load View
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    //MARK: -- Username
+    
+    //MARK: -- Password
+    
+    //MARK: -- Registration
+    
+    //MARK: -- Forgot Password
+    
+    //MARK: -- Sign-In
+    
+    //MARK: -- Sign-Out
+    
+    
+  
+    
+}
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# Porject-Hancock
+# Project-Hancock
+
+### XCODE NOTES ###
+
+
+#### AUTH
+
+I would like the forgot a password to be a popup screen that asks for the users email address.
+
+### SERVER NOTES ###
