b124ce9d36e0057fc0fae9be309d7879247a7857
Lauren Matthews <laurenchmatthews@gmail.com>
2020-07-16 18:08:55 -0600

    Letter d, matching, matching menu
    Fixed the sequence of dots on letter d, finalized constraints for the letter matching menu and letter matching page. Added code to simplify the lettermatchingimage array and code to ensure that there are at least three to four images that match the instruction image

--- Hancock/LetterMatchingViewController.swift
+++ Hancock/LetterMatchingViewController.swift
@@ -9,10 +9,11 @@
 import UIKit
 
 class LetterMatchingViewController: UIViewController, UIGestureRecognizerDelegate {
-      
+    
+    
     //setting up instruction label
-    @IBOutlet weak var instructionLabel: UILabel!
-                //setting up image views - named by row and column position - LM
+    
+    //setting up image views - named by row and column position - LM
     //Row one
     @IBOutlet weak var rowOneCardOneImageView: UIImageView!
     @IBOutlet weak var rowOneCardTwoImageView: UIImageView!
@@ -49,75 +50,70 @@ class LetterMatchingViewController: UIViewController, UIGestureRecognizerDelegat
     
     //tappedImage variable setup
     var tappedImage:UIImage? = nil
-
+    
     //setting up image array using an image literal
-    let letterMatchingArray = [ #imageLiteral(resourceName: "a-"), #imageLiteral(resourceName: "c-"), #imageLiteral(resourceName: "d-"), #imageLiteral(resourceName: "e-"), #imageLiteral(resourceName: "x-"), #imageLiteral(resourceName: "r-") ]
+    let letterMatchingArray = [ #imageLiteral(resourceName: "a-"), #imageLiteral(resourceName: "c-"), #imageLiteral(resourceName: "d-"), #imageLiteral(resourceName: "e-"), #imageLiteral(resourceName: "x-"), #imageLiteral(resourceName: "r-") ,#imageLiteral(resourceName: "b-"), #imageLiteral(resourceName: "f-"), #imageLiteral(resourceName: "g-"), #imageLiteral(resourceName: "h-.png"), #imageLiteral(resourceName: "i-"), #imageLiteral(resourceName: "j-.png"), #imageLiteral(resourceName: "k-.png"), #imageLiteral(resourceName: "l-.png"),#imageLiteral(resourceName: "m-.png"), #imageLiteral(resourceName: "n-"), #imageLiteral(resourceName: "o-.png"), #imageLiteral(resourceName: "p-.png"), #imageLiteral(resourceName: "q-.png"), #imageLiteral(resourceName: "t-.png"), #imageLiteral(resourceName: "s-.png"), #imageLiteral(resourceName: "u-.png"), #imageLiteral(resourceName: "v-.png"), #imageLiteral(resourceName: "w-.png"), #imageLiteral(resourceName: "y-.png"), #imageLiteral(resourceName: "z-.png"), #imageLiteral(resourceName: "A.png"), #imageLiteral(resourceName: "B.png"), #imageLiteral(resourceName: "C"), #imageLiteral(resourceName: "D.png"), #imageLiteral(resourceName: "E.png"), #imageLiteral(resourceName: "F.png"), #imageLiteral(resourceName: "G.png"), #imageLiteral(resourceName: "H.png"), #imageLiteral(resourceName: "I.png"), #imageLiteral(resourceName: "J.png"), #imageLiteral(resourceName: "K.png"), #imageLiteral(resourceName: "L.png"), #imageLiteral(resourceName: "M.png"), #imageLiteral(resourceName: "N.png"), #imageLiteral(resourceName: "O.png"), #imageLiteral(resourceName: "P.png"), #imageLiteral(resourceName: "Q.png"), #imageLiteral(resourceName: "R.png"), #imageLiteral(resourceName: "S.png"), #imageLiteral(resourceName: "T.png"), #imageLiteral(resourceName: "U.png"), #imageLiteral(resourceName: "V.png"), #imageLiteral(resourceName: "W.png"), #imageLiteral(resourceName: "X.png"), #imageLiteral(resourceName: "Y.png"), #imageLiteral(resourceName: "Z.png")]
     
- 
     
-     override func viewDidLoad() {
+    
+    override func viewDidLoad() {
         super.viewDidLoad()
-                      
-        //set instruction label to show instruction image
-        //instructionLabel.text = "test"
         
-        //setup image on cards
-        //row 1
-        rowOneCardOneImageView.image =  letterMatchingArray.randomElement()
-        rowOneCardTwoImageView.image =   letterMatchingArray.randomElement()
-        rowOneCardThreeImageView.image =   letterMatchingArray.randomElement()
-        rowOneCardFourImageView.image =   letterMatchingArray.randomElement()
-        rowOneCardFiveImageView.image =   letterMatchingArray.randomElement()
-        //row two
-        rowTwoCardOneImageView.image =   letterMatchingArray.randomElement()
-        rowTwoCardTwoImageView.image =   letterMatchingArray.randomElement()
-        rowTwoCardThreeImageView.image =   letterMatchingArray.randomElement()
-        rowTwoCardFourImageView.image =   letterMatchingArray.randomElement()
-        rowTwoCardFiveImageView.image =   letterMatchingArray.randomElement()
-        //row three
-        rowThreeCardOneImageView.image =   letterMatchingArray.randomElement()
-        rowThreeCardTwoImageView.image =   letterMatchingArray.randomElement()
-        rowThreeCardThreeImageView.image =   letterMatchingArray.randomElement()
-        rowThreeCardFourImageView.image =   letterMatchingArray.randomElement()
-        rowThreeCardFiveImageView.image =   letterMatchingArray.randomElement()
-        //row four
-        rowFourCardOneImageView.image =   letterMatchingArray.randomElement()
-        rowFourCardTwoImageView.image =   letterMatchingArray.randomElement()
-        rowFourCardThreeImageView.image =   letterMatchingArray.randomElement()
-        rowFourCardFourImageView.image =   letterMatchingArray.randomElement()
-        rowFourCardFiveImageView.image =   letterMatchingArray.randomElement()
-        //row five
-        rowFiveCardOneImageView.image = letterMatchingArray.randomElement()
-        rowFiveCardTwoImageView.image = letterMatchingArray.randomElement()
-        rowFiveCardThreeImageView.image = letterMatchingArray.randomElement()
-        rowFiveCardFourImageView.image = letterMatchingArray.randomElement()
-        rowFiveCardFiveImageView.image = letterMatchingArray.randomElement()
-        //instruction image
-        instructionImageView.image =  letterMatchingArray.randomElement()
+        //set instruction label to show instruction image
+        
         //end of image setup
-        //resetting the opacity of the tapped image when reset is hit
+        //resetting the opacity of the tapped image when reset is hit*/
         
         //set up array of image views
-     let imageViews = [rowOneCardOneImageView, rowOneCardTwoImageView, rowOneCardThreeImageView, rowOneCardFourImageView, rowOneCardFiveImageView, rowTwoCardOneImageView, rowTwoCardTwoImageView, rowTwoCardThreeImageView, rowTwoCardFourImageView, rowTwoCardFiveImageView, rowThreeCardOneImageView, rowThreeCardTwoImageView, rowThreeCardThreeImageView, rowThreeCardFourImageView, rowThreeCardFiveImageView, rowFourCardOneImageView, rowFourCardTwoImageView, rowFourCardThreeImageView, rowFourCardFourImageView, rowFourCardFiveImageView, rowFiveCardOneImageView, rowFiveCardTwoImageView, rowFiveCardThreeImageView, rowFiveCardFourImageView, rowFiveCardFiveImageView]
-     // 2
-     for  imageView in imageViews {
-       // 3
-       let tapGesture = UITapGestureRecognizer(
-         target: self,
-         action: #selector(matchTapped)
-                    
-       )
-
-       // 4
-       tapGesture.delegate = self
-        imageView?.addGestureRecognizer(tapGesture)
-        imageView?.alpha = 1.00
-
-     }
+        instructionImageView.image =  letterMatchingArray.randomElement()
         
-               
         
-}// End of ViewDidLoad
+        let imageViews = [rowOneCardOneImageView, rowOneCardTwoImageView, rowOneCardThreeImageView, rowOneCardFourImageView, rowOneCardFiveImageView, rowTwoCardOneImageView, rowTwoCardTwoImageView, rowTwoCardThreeImageView, rowTwoCardFourImageView, rowTwoCardFiveImageView, rowThreeCardOneImageView, rowThreeCardTwoImageView, rowThreeCardThreeImageView, rowThreeCardFourImageView, rowThreeCardFiveImageView, rowFourCardOneImageView, rowFourCardTwoImageView, rowFourCardThreeImageView, rowFourCardFourImageView, rowFourCardFiveImageView, rowFiveCardOneImageView, rowFiveCardTwoImageView, rowFiveCardThreeImageView, rowFiveCardFourImageView, rowFiveCardFiveImageView]
+        
+        var imageViewsCopy = imageViews
+        
+        var tempImages = [UIImage]()
+        
+        for (index, imageView) in imageViews.enumerated(){
+            imageView?.image = letterMatchingArray.randomElement()
+            if imageView!.image == instructionImageView.image {
+                tempImages.append(imageView!.image!)
+                if index >= 0 && index < imageViewsCopy.count {
+                      imageViewsCopy.remove(at: index)
+                }
+            }
+        }
+        
+        let imageCount = tempImages.count
+        var numberOfDuplicates = 3
+        
+        if imageCount < 3 {
+            var count = numberOfDuplicates - imageCount
+            for n in 0...count {
+                let index = imageViewsCopy.indices.randomElement()
+                imageViews[index!]!.image = instructionImageView.image
+            }
+        }
+        
+        // 2
+        for  imageView in imageViews {
+            // 3
+            let tapGesture = UITapGestureRecognizer(
+                target: self,
+                action: #selector(matchTapped)
+                
+            )
+            
+            // 4
+            tapGesture.delegate = self
+            imageView?.addGestureRecognizer(tapGesture)
+            imageView?.alpha = 1.00
+            
+        }
+        
+        
+        
+    }// End of ViewDidLoad
     
     @IBAction func backButtonTapped(_ sender: Any) {
         performSegue(withIdentifier: "mainMenu", sender: self)
@@ -133,16 +129,16 @@ class LetterMatchingViewController: UIViewController, UIGestureRecognizerDelegat
         
         let tappedImageView = sender.view as! UIImageView
         
-         let imageViews = [rowOneCardOneImageView, rowOneCardTwoImageView, rowOneCardThreeImageView, rowOneCardFourImageView, rowOneCardFiveImageView, rowTwoCardOneImageView, rowTwoCardTwoImageView, rowTwoCardThreeImageView, rowTwoCardFourImageView, rowTwoCardFiveImageView, rowThreeCardOneImageView, rowThreeCardTwoImageView, rowThreeCardThreeImageView, rowThreeCardFourImageView, rowThreeCardFiveImageView, rowFourCardOneImageView, rowFourCardTwoImageView, rowFourCardThreeImageView, rowFourCardFourImageView, rowFourCardFiveImageView]
+        let imageViews = [rowOneCardOneImageView, rowOneCardTwoImageView, rowOneCardThreeImageView, rowOneCardFourImageView, rowOneCardFiveImageView, rowTwoCardOneImageView, rowTwoCardTwoImageView, rowTwoCardThreeImageView, rowTwoCardFourImageView, rowTwoCardFiveImageView, rowThreeCardOneImageView, rowThreeCardTwoImageView, rowThreeCardThreeImageView, rowThreeCardFourImageView, rowThreeCardFiveImageView, rowFourCardOneImageView, rowFourCardTwoImageView, rowFourCardThreeImageView, rowFourCardFourImageView, rowFourCardFiveImageView]
         
         print(sender.view!)
         print("I have been tapped")
-      
+        
         
         //if rowOneCardOneImageView.image == instructionImageView.image{
         //    print("you win")
         //    rowOneCardOneImageView.image = #imageLiteral(resourceName: "Coin")
-       // }//testing getting reference
+        // }//testing getting reference
         
         for _ in imageViews {
             if tappedImageView.image == instructionImageView.image {
@@ -152,12 +148,12 @@ class LetterMatchingViewController: UIViewController, UIGestureRecognizerDelegat
             }
             for _ in imageViews {
                 if tappedImageView.image != instructionImageView.image {
-                   // tappedImageView.alpha = 0.10
+                    // tappedImageView.alpha = 0.10
                     //print("you lose")
                 }
             }
-          
+            
         }
-}//end of matchTapped
+    }//end of matchTapped
     
 }//end of LetterMatchingViewController Class
