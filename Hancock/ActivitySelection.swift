9aefcd2cb94b298b60272d33d4aa19ae2b3c65a2
Jasmine Young <jasmineyoung423@u.boisestate.edu>
2020-08-26 14:56:34 -0600

    fix activty "m" dot and coin points

--- Hancock/ActivitySelection.swift
+++ Hancock/ActivitySelection.swift
@@ -788,25 +788,20 @@ public class ActivitySelection {
         print("Called loadActivitym")
         
         //Line #1
-        activityPoints = [[CGFloat(0.14),CGFloat(0.12)], //first point x, y
-                          [CGFloat(0.14),CGFloat(0.35)], //first MidPoint1 x, y
-                          [CGFloat(0.14),CGFloat(0.6)], //first MidPoint2 x, y
-                          [CGFloat(0.14),CGFloat(0.86)], //second point x, y
+        activityPoints = [[CGFloat(0.24),CGFloat(0.51)], //first point x, y
+                          [CGFloat(0.24),CGFloat(0.63)], //first MidPoint1 x, y
+                          [CGFloat(0.24),CGFloat(0.76)], //first MidPoint2 x, y
+                          [CGFloat(0.24),CGFloat(0.90)], //second point x, y
                           //Line #2
-                          [CGFloat(0.14),CGFloat(0.11)], //fifth point
-                          [CGFloat(0.24),CGFloat(0.35)], //fifth MidPoint1 x, y
-                          [CGFloat(0.37),CGFloat(0.6)], //fifth MidPoint2 x, y
-                          [CGFloat(0.49),CGFloat(0.86)], //sixth point
+                          [CGFloat(0.24),CGFloat(0.54)], //fifth point
+                          [CGFloat(0.45),CGFloat(0.50)], //fifth MidPoint1 x, y
+                          [CGFloat(0.53),CGFloat(0.68)], //fifth MidPoint2 x, y
+                          [CGFloat(0.53),CGFloat(0.90)], //sixth point
                           //Line #3
-                          [CGFloat(0.49),CGFloat(0.86)], //fifth point
-                          [CGFloat(0.61),CGFloat(0.6)], //fifth MidPoint1 x, y
-                          [CGFloat(0.75),CGFloat(0.35)], //fifth MidPoint2 x, y
-                          [CGFloat(0.87),CGFloat(0.12)], //sixth point
-                          //Line #4
-                          [CGFloat(0.87),CGFloat(0.12)], //fifth point
-                          [CGFloat(0.87),CGFloat(0.35)], //fifth MidPoint1 x, y
-                          [CGFloat(0.87),CGFloat(0.6)], //fifth MidPoint2 x, y
-                          [CGFloat(0.87),CGFloat(0.86)]] //sixth point
+                          [CGFloat(0.49),CGFloat(0.54)], //fifth point
+                          [CGFloat(0.70),CGFloat(0.50)], //fifth MidPoint1 x, y
+                          [CGFloat(0.78),CGFloat(0.68)], //fifth MidPoint2 x, y
+                          [CGFloat(0.78),CGFloat(0.90)]] //sixth point
         
         let Underlaym = UIImage(named: "art.scnassets/LetterImages/m-.png")
         letterUnderlay = UIImageView(image: Underlaym)
